611f80fc83b2c825186996e49796973a7cc7f484
vmatuka23 <vmatuka23@student.foi.hr>
2026-01-26 20:52:17 +0100

    Dodana inicijalizacija sesija i sređen package.json

--- src/servis/server.ts
+++ src/servis/server.ts
@@ -3,18 +3,10 @@ import express, { Application } from "express";
 import { dajPort } from "../zajednicko/esmPomocnik.js";
 import { Konfiguracija } from "../zajednicko/konfiguracija.js";
 import { pripremiPutanjeResursTMDB, pripremiPutanjeResursKorisnika } from "./servis.js";
+import session from "express-session";
+import cookieParser from "cookie-parser";
 
 const server: Application = express();
-
-
-function pokreniServer(server: Application, port: number) {
-    server.listen(port, () => {
-        console.log(`Server pokrenut na portu: ${port}`);
-    });
-    
-    // Keep the process alive
-    setInterval(() => {}, 1000);
-}
 
 function inicijalizirajPostavkeServera(server: Application) {
     server.use(express.urlencoded({ extended: true }));
@@ -30,6 +22,22 @@ function inicijalizirajPostavkeServera(server: Application) {
             }
         },
         optionsSuccessStatus: 200
+    }));
+}
+
+function inicijalizirajSesiju(server: Application, konf: Konfiguracija) {
+    server.use(cookieParser());
+
+    // session middleware
+    server.use(session({
+        secret: konf.dajKonf().tajniKljucSesija,
+        resave: false,
+        saveUninitialized: false, // Ne kreiraj prazne sesije
+        cookie: {
+            httpOnly: true,
+            secure: false, // true ako je HTTPS, na localhost treba false
+            maxAge: 1000 * 60 * 60 // 1 sat
+        }
     }));
 }
 
@@ -49,14 +57,23 @@ function pripremiPutanjeServera(server: Application, konf: Konfiguracija) {
     });
 }
 
+function pokreniServer(server: Application, port: number) {
+    server.listen(port, () => {
+        console.log(`Server pokrenut na portu: ${port}`);
+    });
+    
+    // Keep the process alive
+    setInterval(() => {}, 1000);
+}
 
 async function main(argv: Array<string>) {
-    let port: number = dajPort("vmatuka");
+    let port: number = dajPort("vmatuka23");
     if (argv[3] != undefined) {
         port = parseInt(argv[3]);
     }
 
     let konf: Konfiguracija | null = null;
+
     try {
         konf = await inicijalizirajKonfiguraciju();
     } catch (greska: Error | any) {
@@ -72,6 +89,7 @@ async function main(argv: Array<string>) {
 
     inicijalizirajPostavkeServera(server);
     if (konf !== null){
+        inicijalizirajSesiju(server, konf);
         pripremiPutanjeServera(server, konf);
     }
     pokreniServer(server, port);
